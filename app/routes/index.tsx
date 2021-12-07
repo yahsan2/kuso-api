25c1743f8c9d3d3602b11be007ac7492e457dba7
Ryosuke Miyamoto <yahsan2@gmail.com>
2021-12-07 14:42:30 +0900

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -31,6 +31,11 @@ export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
     description: "Welcome to remix!",
+    "og:title": "Kuso API",
+    "og:description": "Welcome to remix!",
+    "og:url": "https://kuso-api.vercel.app",
+    "og:image": "https://kuso-api.vercel.app/kuso-ogp.jpg",
+    "twitter:image": "summary_large_image",
   };
 };
 
@@ -175,10 +180,21 @@ export default function Index() {
             <span className="text-xs text-blue-400 font-semibold">
               API sample
             </span>
+            <div className="relative flex flex-wrap mt-10 mb-6">
+              <input
+                className="relative mb-2 md:mb-0 w-full py-4 pl-4 text-sm border rounded"
+                type="text"
+                readOnly
+                value="https://api.sssapi.app/wAR1fWuTyRyQJxLPzpqQf"
+              />
+              <span className="absolute top-0 left-0 ml-4 -mt-2 px-1 inline-block bg-white text-gray-500 text-xs">
+                kuso API
+              </span>
+            </div>
           </div>
-          <div className="flex flex-wrap -mx-2 -mb-12 lg:-mb-16">
+          <div className="md:masonry-2-col lg:masonry-3-col box-border mx-auto before:box-inherit after:box-inherit">
             {items.reverse().map((item) => (
-              <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-12 lg:mb-16">
+              <div className="break-inside mb-4">
                 <div className="border border-red-200 px-4 py-4 rounded-lg">
                   <span className="inline-block mb-4 text-xs text-gray-500">
                     {item.createdAt}
@@ -203,6 +219,17 @@ export default function Index() {
             ))}
           </div>
         </div>
+        <div className="text-center block mt-4">
+          <a
+            className="py-4 px-6 inline-block text-sm text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200"
+            data-config-id="primary-action"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdz5yYrR6vmUeUC_kUg7OmssOCVfxzsS3__GJ6gdWRrWsiL-w/viewform"
+            target="_blank"
+          >
+            Add your "kuso" data.
+          </a>
+          <p className="text-xs text-gray-600 mt-2">Google Form に飛びます。</p>
+        </div>
       </section>
       <section className="relative py-20">
         <img
@@ -219,8 +246,9 @@ export default function Index() {
           <div className="max-w-2xl mx-auto mb-20 text-center">
             <span className="text-xs text-blue-400 font-semibold">Price</span>
             <h2 className="mt-8 mb-10 text-4xl font-semibold font-heading">
-              This is an amazing api because it's made with remixjs!
-              <br />I just wanted to create app based on remix :D
+              Kuso API is soooo expensive!
+              <br />
+              But if you are a kuso professional, you can donate me :)
             </h2>
             <div className="max-w-max mx-auto p-1 bg-gray-50 rounded">
               <button className="px-10 py-4 bg-white rounded text-sm font-semibold leading-normal">
@@ -278,7 +306,7 @@ export default function Index() {
                         fill="#45C1FF"
                       />
                     </svg>
-                    <p className="font-medium">Working materials in Figma</p>
+                    <p className="font-medium">Working materials</p>
                   </li>
                   <li className="flex items-center py-4 border-b">
                     <svg
@@ -296,7 +324,7 @@ export default function Index() {
                         fill="#45C1FF"
                       />
                     </svg>
-                    <p className="font-medium">100GB cloud passtion</p>
+                    <p className="font-medium">100GB cloud spirit</p>
                   </li>
                   <li className="flex items-center py-4 mb-8">
                     <svg
@@ -321,7 +349,7 @@ export default function Index() {
                   className="block py-4 text-sm text-center text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200"
                   href=""
                 >
-                  Coming soon
+                  Never Coming soon
                 </a>
               </div>
             </div>
@@ -330,10 +358,10 @@ export default function Index() {
                 <h3 className="mb-6 font-medium text-red-300">Pro</h3>
                 <div className="flex lg:justify-center mb-8">
                   <span className="self-start inline-block mr-1 text-xl font-semibold text-red-300">
-                    $
+                    ￥
                   </span>
                   <p className="self-end text-5xl font-semibold text-white">
-                    6599,99
+                    300
                   </p>
                 </div>
                 <ul className="mb-6 text-left">
@@ -354,7 +382,7 @@ export default function Index() {
                       />
                     </svg>
                     <p className="font-medium text-white">
-                      Complete documentation
+                      私が開発・運営してる poiit.me で投げ銭すると
                     </p>
                   </li>
                   <li className="flex items-center py-4 border-b border-red-400">
@@ -374,7 +402,7 @@ export default function Index() {
                       />
                     </svg>
                     <p className="font-medium text-white">
-                      Working materials in Figma
+                      Complete documentation はもらえませんし、
                     </p>
                   </li>
                   <li className="flex items-center py-4 border-b border-red-400">
@@ -394,7 +422,7 @@ export default function Index() {
                       />
                     </svg>
                     <p className="font-medium text-white">
-                      100GB cloud storage
+                      500 team members 全員には無理なのですが、
                     </p>
                   </li>
                   <li className="flex items-center py-4 border-b border-red-400">
@@ -413,7 +441,9 @@ export default function Index() {
                         fill="#45C1FF"
                       />
                     </svg>
-                    <p className="font-medium text-white">500 team members</p>
+                    <p className="font-medium text-white">
+                      100GB の愛があるお礼のメッセージで
+                    </p>
                   </li>
                   <li className="flex items-center py-4 mb-8">
                     <svg
@@ -431,11 +461,13 @@ export default function Index() {
                         fill="#45C1FF"
                       />
                     </svg>
-                    <p className="font-medium text-white">Premium support</p>
+                    <p className="font-medium text-white">
+                      あなたに Premium 感謝します
+                    </p>
                   </li>
                 </ul>
-                <button className="inline-block py-4 w-full text-sm text-center font-medium leading-normal text-white rounded bg-blue-400 hover:bg-blue-500 transition duration-200">
-                  Coming soon
+                <button className="inline-block py-4 w-full text-sm text-center font-medium leading-normal text-white rounded bg-purple-400 hover:bg-purple-500 transition duration-200">
+                  [PR] poiit.me で投げ銭する
                 </button>
               </div>
             </div>
@@ -485,7 +517,7 @@ export default function Index() {
                         fill="#45C1FF"
                       />
                     </svg>
-                    <p className="font-medium">Working materials in Figma</p>
+                    <p className="font-medium">Working materials</p>
                   </li>
                   <li className="flex items-center py-4 pl-8 border-b">
                     <svg
@@ -505,7 +537,7 @@ export default function Index() {
                     </svg>
                     <p className="font-medium">100GB cloud storage</p>
                   </li>
-                  <li className="flex items-center py-4 mb-8 pl-8">
+                  <li className="flex items-center py-4 pl-8 border-b">
                     <svg
                       className="mr-2"
                       width={20}
@@ -523,12 +555,30 @@ export default function Index() {
                     </svg>
                     <p className="font-medium">500 team members</p>
                   </li>
+                  <li className="flex items-center py-4 mb-8 pl-8">
+                    <svg
+                      className="mr-2"
+                      width={20}
+                      height={20}
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.67 0H14.34C17.73 0 20 2.38 20 5.92V14.091C20 17.62 17.73 20 14.34 20H5.67C2.28 20 0 17.62 0 14.091V5.92C0 2.38 2.28 0 5.67 0ZM9.43 12.99L14.18 8.24C14.52 7.9 14.52 7.35 14.18 7C13.84 6.66 13.28 6.66 12.94 7L8.81 11.13L7.06 9.38C6.72 9.04 6.16 9.04 5.82 9.38C5.48 9.72 5.48 10.27 5.82 10.62L8.2 12.99C8.37 13.16 8.59 13.24 8.81 13.24C9.04 13.24 9.26 13.16 9.43 12.99Z"
+                        fill="#45C1FF"
+                      />
+                    </svg>
+                    <p className="font-medium">Premium Support</p>
+                  </li>
                 </ul>
                 <a
                   className="inline-block py-4 w-full text-sm text-center font-medium leading-normal text-white rounded bg-red-400 hover:bg-red-300 transition duration-200"
                   href=""
                 >
-                  Coming soon
+                  Never Coming soon
                 </a>
               </div>
             </div>
@@ -545,21 +595,30 @@ export default function Index() {
           <div className="max-w-2xl mx-auto mb-20 text-center">
             <span className="text-xs text-blue-400 font-semibold"> Docs</span>
             <h2 className="mt-8 mb-10 text-4xl font-semibold font-heading">
-              This API is created by using SSSAPI.
-              <br />
-              Please check{" "}
+              This API is created by using{" "}
               <a
                 href="https://sssapi.app/"
                 target="_blank"
-                className="bg-red-400  hover:bg-red-300"
+                className="ml-1 bg-red-400  hover:bg-red-300"
               >
-                SSSAPI site.
+                SSSAPI
               </a>
+              .
+              <br />
+              Please check the
+              <a
+                href="https://sssapi.app/"
+                target="_blank"
+                className="ml-1 bg-red-400  hover:bg-red-300"
+              >
+                website
+              </a>
+              .
             </h2>
           </div>
           <div className="">
             <p className="text-xs">
-              iframe で
+              このサイトはSSSAPIを利用しています。以下は、iframe で
               <a href="https://console.sssapi.app/tools/testRun?u=https%3A%2F%2Fapi.sssapi.app%2FwAR1fWuTyRyQJxLPzpqQf">
                 SSSAPI のページ
               </a>
